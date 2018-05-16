4d0812616fb177ee4e180eb91eadbfc75b8b6e7c
anthousecode <bsmgroup69@gmail.com>
2018-05-16 14:00:56 +0300

    added Event create routs

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-14T14:12:26.213Z",
+    "time": "2018-05-16T10:59:16.338Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
