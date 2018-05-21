138bb50c2d25d5a1f2edd9d2ffead4c573694aeb
anthousecode <bsmgroup69@gmail.com>
2018-05-21 15:12:02 +0300

    Added comment route

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-21T11:43:46.808Z",
+    "time": "2018-05-21T12:10:37.261Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
