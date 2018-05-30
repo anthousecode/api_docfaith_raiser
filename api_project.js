1695813af2ffdfbf3b05d054757a4d756ab7e34f
anthousecode <bsmgroup69@gmail.com>
2018-05-30 16:06:05 +0300

    Update API DOC

--- api_project.js
+++ api_project.js
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-30T12:21:52.786Z",
+    "time": "2018-05-30T13:02:12.208Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
